67544640164bcb9742a6628525e2c51949896155
Vallor23 <enosnyabinda2017@gmail.com>
2024-08-08 17:48:58 +0300

    Update src/App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WelcomeMessage from './WelcomeMessage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <WelcomeMessage />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -17,6 +19,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h1>hello world</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
