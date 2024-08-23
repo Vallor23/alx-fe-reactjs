0a19654bbe140971b4cf6f11e7dfc925a0ad469d
Vallor23 <enosnyabinda2017@gmail.com>
2024-08-23 11:03:34 +0300

    Index modified

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,5 @@
 //RecipeList component
-import { useRecipeStore } from './recipeStore';
+import useRecipeStore from "./RecipeStore";
 
 const RecipeList = () => {
     const recipes = useRecipeStore(state => state.recipes);
--- recipe-sharing-app/src/components/RecipeStore.js
+++ recipe-sharing-app/src/components/RecipeStore.js
@@ -1,10 +1,12 @@
-import create from 'zustand';
+import {create} from 'zustand';
 
  const useRecipeStore = create(set => ({
-    recipe: [],
+    recipes: [],
     addRecipe:(newRecipe) =>(state => ({ recipes: [...state.recipes, newRecipe]
     })),
-    setRecipes: (recipes) => set({ recipes})
+    setRecipes: (recipes) => set({ recipes}),
+    deleteRecipe: () =>(state => ({recipe: state.recipe})),
+    
 }))
 
 export default useRecipeStore;
