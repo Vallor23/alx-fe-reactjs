cdf7e1aa72ac3d2d73e14c8500abddf477942090
Vallor23 <enosnyabinda2017@gmail.com>
2024-08-25 15:14:04 +0300

    Add search icon to filter recipe

--- recipe-sharing-app/src/components/SearchBar.jsx
+++ recipe-sharing-app/src/components/SearchBar.jsx
@@ -1,14 +1,24 @@
-import { useRecipeStore } from './RecipeStore';
+import useRecipeStore from './recipeStore';
+import { BsSearch } from 'react-icons/bs';
+import './SearchBar.css';
 
+console.log("SearchBar rendered");
 const SearchBar = () => {
     const setSearchTerm = useRecipeStore(state => state.setSearchTerm);
-
+    const filterRecipes = useRecipeStore(state => state.filterRecipes);
+    
+    console.log("SearchBar rendered");
     return (
-        <input 
-        type="text"
-        placeholder='Search recipes...'
-        onChange={(event) => setSearchTerm(event.target.value)}
-        />
+        <div className='search-bar'>
+            <input 
+            type="text"
+            placeholder='Search recipes...'
+            onChange={(event) => setSearchTerm(event.target.value)}//Update the search term when the user types
+            className='search-input'
+            />
+            <BsSearch onClick={filterRecipes} className='search-icon'/>
+        </div>
+        
     );
 }
 export default SearchBar;
