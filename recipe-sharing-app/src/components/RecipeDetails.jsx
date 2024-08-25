204534f792d8f79b896ca32680cea93fd9178dd5
Vallor23 <enosnyabinda2017@gmail.com>
2024-08-25 15:12:03 +0300

    Modified recipe details

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,6 +1,6 @@
 // RecipeDetails component
 import { useParams } from 'react-router-dom';
-import useRecipeStore  from './RecipeStore';
+import useRecipeStore  from './recipeStore';
 import EditRecipeForm from './EditRecipeForm';
 import DeleteRecipeButton from './DeleteRecipeButton';
 
@@ -18,8 +18,10 @@ const RecipeDetails = () => {
 
     return (
         <div>
-            <p>{`My recipe id: ${recipe.id}`}</p>
             <h1>{recipe.title}</h1>
+            <p>{recipe.cuisine}</p>
+            <p>{recipe.difficulty}</p>
+            <p>{recipe.cookingTime}</p>
             <p>{recipe.description}</p>
             {/* Render EditRecipeForm and DeleteRecipeButton here */}
             <EditRecipeForm />
