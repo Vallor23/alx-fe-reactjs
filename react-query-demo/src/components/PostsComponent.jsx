97088248101555e6238e7f30015296eb297fe607
Vallor23 <enosnyabinda2017@gmail.com>
2024-08-29 11:55:29 +0300

    Implement Features for Caching and Updating Data

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 //Fetch function used to fetch data from an API
 const fetchData = async() => {
@@ -7,12 +7,30 @@ const fetchData = async() => {
 };
 
 const PostsComponent = () => {
+    const queryClient = useQueryClient();//Initialize query client
+
     //useQuery hook to handle data fetching,loading states, error handling, and data caching.
     const {data, error, isLoading} = useQuery('fetchData', fetchData);
+    
+    // Use useMutation to handle manual refetching of data
+    const mutation = useMutation({
+        mutationFn: fetchData,
+        onSuccess: () => {
+            // Invalidate and refetch the fetchData query
+            queryClient.invalidateQueries('[fetchData]')
+        }
+    });
+    
     //Handle Loading state
     if(isLoading) return <div>Loading...</div>;
     //Handle error state
-    if(error) return <div></div>;
+    if(error) return <div>Error Loading Data</div>;
+
+    
+        //Function to trigger mutation
+        const handleRefetch = () => {
+            mutation.mutate();
+        }
 
     //Render the fetched data
     return (
@@ -22,7 +40,9 @@ const PostsComponent = () => {
                     <div key={item.id}>{item.name}</div>
                 ))}
             </ul>
-            {/* <button onClick={}></button> */}
+            <button onClick={handleRefetch} disabled={mutation.isLoading}>
+                {mutation.isLoading ? 'Refetching...' : 'Refetch Data'}
+            </button>
         </div>
     );
 };
