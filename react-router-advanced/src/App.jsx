45eb04f311173f29358f2b0d24bff2f4b9f0c2c6
Vallor23 <enosnyabinda2017@gmail.com>
2024-08-30 18:29:06 +0300

    Implement Nested routes

---
+++ react-router-advanced/src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+function App () {
+    return (
+        <Router>
+            <Switch>
+                <Route />
+            </Switch>
+        </Router>
+    )
+}
+
+export default App;
---
+++ react-router-advanced/src/components/Profile.jsx
@@ -0,0 +1,36 @@
+import { Route, useRouteMatch, Link, Switch} from "react-router-dom";
+
+function Profile() {
+    //UseRouteMatch provides the current URL and path
+    let {path, url} = useRouteMatch();
+
+    return (
+        <div>
+              <h1>Profile</h1>
+              <ul>
+                <li>
+                    <Link to = {`${url}/ProfileDetails`}>ProfileDetails</Link>
+                </li>
+                <li>
+                    <Link to = {`${url}/ProfileSettings`}>ProfileSettings</Link>
+                </li>
+              </ul>
+              <Switch>
+                <Route exact path={path}>
+                    <h3>Please select an option</h3>
+                </Route>
+                <Route path={`${path}/ProfileDetails`}>
+                    <ProfileDetails />
+                </Route>
+                <Route path={`${path}/ProfileSettings`}>
+                    <ProfileSettings />
+                </Route>
+              </Switch>
+        </div>
+  )
+}
+
+const ProfileDetails = () => <h4>ProfileDetails</h4>
+const ProfileSettings = () => <h4>ProfileSettings</h4>
+
+export default Profile;
