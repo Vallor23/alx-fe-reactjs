685d2152620163f59dfa722430818ad12b0538d3
Vallor23 <enosnyabinda2017@gmail.com>
2024-09-11 13:44:02 +0300

    React Router Integration

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,8 +1,16 @@
-import Homepage from './components/Homepage'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Homepage from './components/Homepage';
+import RecipeDetail from './components/RecipeDetail';
 
 function App() {
   return (
-    <Homepage />
+    <Router>
+      <Routes>
+        <Route path='/' element={<Homepage />}/>
+        <Route path='/recipe/:id' element= {<RecipeDetail />}/>
+      </Routes>
+    </Router>
   )
 }
 
