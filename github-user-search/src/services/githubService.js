ad3ba0fe55727baa3d524f6fe47bc6ca8350a736
Vallor23 <enosnyabinda2017@gmail.com>
2024-09-21 14:53:39 +0300

    Modify  advance API Integration

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
 const fetchUserData = async({username,location,minRepos}) => {
-    let query = `q=${username}`;
+    let query = `${username}`;
     if(location) query += `+location:${location}`;
     if(minRepos) query += `+minRepos:${minRepos}`;
-    const response = await axios.get(`https://api.github.com/search/users?${query}`)
+    const response = await axios.get(`https://api.github.com/search/users?q=${query}`)
     return response.data.items
 }
 
